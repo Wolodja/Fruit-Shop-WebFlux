51b66058683f8680a63dae71e45a640081a48943
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-05-13 18:38:01 +0200

    Vendor information and data population added

---
+++ src/main/java/com/github/spring5webfluxrest/repositories/VendorRepository.java
@@ -0,0 +1,7 @@
+package com.github.spring5webfluxrest.repositories;
+
+import com.github.spring5webfluxrest.domain.Vendor;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface VendorRepository extends ReactiveMongoRepository<Vendor, String> {
+}
