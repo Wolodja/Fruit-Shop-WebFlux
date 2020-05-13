1e8a3517caed7884c4fc44f7f9906aa4fd607545
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-05-13 18:16:02 +0200

    Adding Category and repository

---
+++ src/main/java/com/github/spring5webfluxrest/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.github.spring5webfluxrest.repositories;
+
+import com.github.spring5webfluxrest.domain.Category;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+
+public interface CategoryRepository extends ReactiveMongoRepository<Category, String> {
+}
